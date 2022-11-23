a38232e7aeab71c80648942d6d7e005ed4f5aee6
qigel <lenshtein-z@yandex.ru>
2022-11-23 13:32:57 +0300

    Add files via upload

--- LibToJson.py
+++ LibToJson.py
@@ -1,141 +1,98 @@
 from liberty.parser import parse_liberty
-from liberty.types import EscapedString
+from liberty.types import *
 import json
+import ast
 
 
 def encode_escaped_string(s):
     if isinstance(s, EscapedString):
-        return s.value
+        return str(s)
+    elif isinstance(s, WithUnit):
+        return str(s)
+    elif isinstance(s, ArithExpression):
+        return str(s)
+    elif isinstance(s, NameBitSelection):
+        return str(s)
     else:
         type_name = s.__class__.__name__
         raise TypeError(f"Object of type '{type_name}' is not JSON serializable")
 
 
-def lib_to_json(liberty_file, json_file):
-    library = parse_liberty(open(liberty_file).read())
-    JSON_dict = dict()
+def escaped_string_convert(obj):
+    if isinstance(obj, Group):
+        new_obj = Group(escaped_string_convert(obj.group_name))
+        for group in obj.groups:
+            group = escaped_string_convert(group)
+            new_obj.groups.append(group)
+        for attr in obj.attributes:
+            attr = escaped_string_convert(attr)
+            new_obj.attributes.append(attr)
+        for arg in obj.args:
+            arg = escaped_string_convert(arg)
+            new_obj.args.append(arg)
+        for define in obj.defines:
+            define = escaped_string_convert(define)
+            new_obj.defines.append(define)
+        obj = new_obj
+    elif isinstance(obj, Attribute):
+        obj.value = escaped_string_convert(obj.value)
+        obj.name = escaped_string_convert(obj.name)
+    elif isinstance(obj, Define):
+        obj.attribute_name = escaped_string_convert(obj.attribute_name)
+        obj.attribute_type = escaped_string_convert(obj.attribute_type)
+        obj.group_name = escaped_string_convert(obj.group_name)
+    elif isinstance(obj, EscapedString):
+        obj = str(obj)
+    elif isinstance(obj, list):
+        if isinstance(obj[0], EscapedString):
+            try:
+                obj = ast.literal_eval(str(obj))
+            except:
+                new_obj = list()
+                for el in obj:
+                    new_obj.append(escaped_string_convert(el))
+                obj = new_obj
+    return obj
+
+
+def group_to_dict(group):
+    out_dict = dict()
     attr_cnt = dict()
-    for attr in library.attributes:
+    for attr in group.attributes:
         if attr_cnt.get(attr.name) is None:
             attr_cnt[attr.name] = 1
         else:
             attr_cnt[attr.name] += 1
-    for attr in library.attributes:
+    for attr in group.attributes:
         if attr_cnt[attr.name] == 1:
-            JSON_dict[attr.name] = attr.value
+            out_dict[attr.name] = attr.value
         else:
-            if JSON_dict.get('comp_attribute,' + attr.name) is None:
-                JSON_dict['comp_attribute,' + attr.name] = list()
-            JSON_dict['comp_attribute,' + attr.name].append(attr.value)
-    JSON_dict['define'] = list()
-    for define in library.defines:
-        JSON_dict['define'].append(
-            dict(attribute_name=define.attribute_name, attribute_type=define.attribute_type,
-                 group_name=define.group_name))
-    for group in library.groups:
-        if group.group_name != 'cell':
-            JSON_dict[group.group_name + ',' + group.args[0].value] = dict()
-            attr_cnt = dict()
-            for attr in group.attributes:
-                if attr_cnt.get(attr.name) is None:
-                    attr_cnt[attr.name] = 1
-                else:
-                    attr_cnt[attr.name] += 1
-            for attr in group.attributes:
-                if attr_cnt[attr.name] == 1:
-                    JSON_dict[group.group_name + ',' + group.args[0].value][attr.name] = attr.value
-                else:
-                    if JSON_dict[group.group_name + ',' + group.args[0].value].get(
-                            'comp_attribute,' + attr.name) is None:
-                        JSON_dict[group.group_name + ',' + group.args[0].value]['comp_attribute,' + attr.name] = list()
-                    JSON_dict[group.group_name + ',' + group.args[0].value]['comp_attribute,' + attr.name].append(
-                        attr.value)
-    JSON_dict['cells'] = dict()
-    for cell in library.groups:
-        if cell.group_name == 'cell':
-            JSON_dict['cells'][cell.args[0].value] = dict()
-            attr_cnt = dict()
-            for attr in cell.attributes:
-                if attr_cnt.get(attr.name) is None:
-                    attr_cnt[attr.name] = 1
-                else:
-                    attr_cnt[attr.name] += 1
-            for attr in cell.attributes:
-                if attr_cnt[attr.name] == 1:
-                    JSON_dict['cells'][cell.args[0].value][attr.name] = attr.value
-                else:
-                    if JSON_dict['cells'][cell.args[0].value].get('comp_attribute,' + attr.name) is None:
-                        JSON_dict['cells'][cell.args[0].value]['comp_attribute,' + attr.name] = list()
-                    JSON_dict['cells'][cell.args[0].value]['comp_attribute,' + attr.name].append(attr.value)
-            for group in cell.groups:
-                if group.args[0] is None:
-                    if JSON_dict['cells'][cell.args[0].value].get(group.group_name) is None:
-                        JSON_dict['cells'][cell.args[0].value][group.group_name] = list()
-                    JSON_dict['cells'][cell.args[0].value][group.group_name].append(dict())
-                    attr_cnt = dict()
-                    for attr in group.attributes:
-                        if attr_cnt.get(attr.name) is None:
-                            attr_cnt[attr.name] = 1
-                        else:
-                            attr_cnt[attr.name] += 1
-                    for attr in group.attributes:
-                        if attr_cnt[attr.name] == 1:
-                            JSON_dict['cells'][cell.args[0].value][group.group_name][-1][attr.name] = attr.value
-                        else:
-                            if JSON_dict['cells'][cell.args[0].value][group.group_name][-1].get(
-                                    'comp_attribute,' + attr.name) is None:
-                                JSON_dict['cells'][cell.args[0].value][group.group_name][-1][
-                                    'comp_attribute,' + attr.name] = list()
-                            JSON_dict['cells'][cell.args[0].value][group.group_name][-1][
-                                'comp_attribute,' + attr.name].append(attr.value)
-                else:
-                    JSON_dict['cells'][cell.args[0].value][group.group_name + ',' + group.args[0].value] = dict()
-                    attr_cnt = dict()
-                    for attr in group.attributes:
-                        if attr_cnt.get(attr.name) is None:
-                            attr_cnt[attr.name] = 1
-                        else:
-                            attr_cnt[attr.name] += 1
-                    for attr in group.attributes:
-                        if attr_cnt[attr.name] == 1:
-                            JSON_dict['cells'][cell.args[0].value][group.group_name + ',' + group.args[0].value][
-                                attr.name] = attr.value
-                        else:
-                            if JSON_dict['cells'][cell.args[0].value][group.group_name + ',' + group.args[0].value].get(
-                                    'comp_attribute,' + attr.name) is None:
-                                JSON_dict['cells'][cell.args[0].value][group.group_name + ',' + group.args[0].value][
-                                    'comp_attribute,' + attr.name] = list()
-                            JSON_dict['cells'][cell.args[0].value][group.group_name + ',' + group.args[0].value][
-                                'comp_attribute,' + attr.name].append(attr.value)
-                    for gr1 in group.groups:
-                        JSON_dict['cells'][cell.args[0].value][group.group_name + ',' + group.args[0].value][
-                            gr1.group_name] = dict()
-                        for gr2 in gr1.groups:
-                            JSON_dict['cells'][cell.args[0].value][group.group_name + ',' + group.args[0].value][
-                                gr1.group_name][gr2.group_name + ',' + gr2.args[0].value] = dict()
-                            attr_cnt = dict()
-                            for attr in gr2.attributes:
-                                if attr_cnt.get(attr.name) is None:
-                                    attr_cnt[attr.name] = 1
-                                else:
-                                    attr_cnt[attr.name] += 1
-                            for attr in gr2.attributes:
-                                if attr_cnt[attr.name] == 1:
-                                    JSON_dict['cells'][cell.args[0].value][
-                                        group.group_name + ',' + group.args[0].value][
-                                        gr1.group_name][gr2.group_name + ',' + gr2.args[0].value][
-                                        attr.name] = attr.value
-                                else:
-                                    if JSON_dict['cells'][cell.args[0].value][
-                                        group.group_name + ',' + group.args[0].value][
-                                        gr1.group_name][gr2.group_name + ',' + gr2.args[0].value].get(
-                                        'comp_attribute,' + attr.name) is None:
-                                        JSON_dict['cells'][cell.args[0].value][
-                                            group.group_name + ',' + group.args[0].value][
-                                            gr1.group_name][gr2.group_name + ',' + gr2.args[0].value][
-                                            'comp_attribute,' + attr.name] = list()
-                                    JSON_dict['cells'][cell.args[0].value][
-                                        group.group_name + ',' + group.args[0].value][
-                                        gr1.group_name][gr2.group_name + ',' + gr2.args[0].value][
-                                        'comp_attribute,' + attr.name].append(attr.value)
+            if out_dict.get('comp_attribute,' + str(attr.name)) is None:
+                out_dict['comp_attribute,' + str(attr.name)] = list()
+            out_dict['comp_attribute,' + str(attr.name)].append(attr.value)
+    if len(group.defines) != 0:
+        out_dict['define'] = list()
+        for define in group.defines:
+            out_dict['define'].append(
+                dict(attribute_name=define.attribute_name, attribute_type=define.attribute_type,
+                     group_name=define.group_name))
+    contains_cells = False
+    for cgroup in group.groups:
+        if cgroup.group_name == 'cell':
+            contains_cells = True
+        else:
+            out_dict[str(cgroup.group_name) + ',' + str(cgroup.args[0])] = group_to_dict(cgroup)
+    if contains_cells:
+        out_dict['cells'] = dict()
+        for cell in group.groups:
+            if cell.group_name == 'cell':
+                out_dict['cells'][cell.args[0]] = group_to_dict(cell)
+    return out_dict
+
+
+def lib_to_json(liberty_file, json_file):
+    library = parse_liberty(open(liberty_file).read())
+    library = escaped_string_convert(library)
+    #JSON_dict = dict()
+    JSON_dict = group_to_dict(library)
     json.dump(JSON_dict, open(json_file, 'w'), default=encode_escaped_string)
